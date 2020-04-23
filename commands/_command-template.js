39b3680764a7f797e815ca833c3865c0e5add947
Biagio <biagio.santori@gmail.com>
2020-04-24 00:34:47 +0100

    Basic Addition

---
+++ commands/_command-template.js
@@ -0,0 +1,29 @@
+// Any module required will be written up here
+const Util = require('./../modules/util')
+const Logger = new Util.Logger();
+
+/**
+ * Command: NAME
+ * Description: DESCRIPTION
+ * */
+
+module.exports = {
+	name: 'NAME',
+	description: 'DESCRIPTION',
+	execute(message, args, config) {
+    const command = args[0].slice(config.PREFIX.length,)
+    const rawArgument = args.join(' ')
+    const argument = rawArgument.replace(config.PREFIX + command + ' ', '')
+    
+    // Check in what type of channel the command was executed
+		if(message.channel.type === 'dm' || message.channel.type === 'group') {
+			Logger.info(`${config.PREFIX + this.name} used in a private ${message.channel.type}.`)
+		}
+		else{
+			Logger.info(`${config.PREFIX + this.name} used on ${message.guild.name} (${message.guild.id}; ${message.guild.memberCount} users)`)
+		}
+
+    // Put command here...
+
+	},
+}
